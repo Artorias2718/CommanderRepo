b90ad300ed6bcd8ccd147b54bf6d18ecb689f5c7
Artorias2718 <arturoordonez2718@gmail.com>
2021-11-12 15:13:27 -0700

    Commander Repo -- Part 1

---
+++ Controllers/CommandsController.cs
@@ -0,0 +1,35 @@
+using Microsoft.AspNetCore.Mvc;
+using System.Collections.Generic;
+using Commander.Data;
+using Commander.Models;
+
+namespace Commander.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class CommandsController : ControllerBase
+    {
+        private readonly ICommanderRepo m_oRepository;
+
+        public CommandsController(ICommanderRepo i_oRepository)
+        {
+            m_oRepository = i_oRepository;
+        }
+
+        // GET api/commands
+        [HttpGet]
+        public ActionResult<IEnumerable<Command>> GetAllCommands()
+        {
+            var oCommandItems  = m_oRepository.GetAppCommands();
+            return Ok(oCommandItems);
+        }
+
+        // GET api/commands/{id}
+        [HttpGet("{id}")]
+        public ActionResult<Command> GetCommandById(int id)
+        {
+            var oCommandItem = m_oRepository.GetCommandById(id);
+            return Ok(oCommandItem);
+        }
+    }
+}
---
+++ Data/MockCommanderRepo.cs
@@ -0,0 +1,25 @@
+using System.Collections.Generic;
+using Commander.Models;
+
+namespace Commander.Data
+{
+    public class MockCommanderRepo : ICommanderRepo
+    {
+        public IEnumerable<Command> GetAppCommands()
+        {
+            var commands = new List<Command>()
+            {
+                new Command { Id = 0, HowTo = "Boil an Egg", Line = "Boil Water", Platform = "Kettle & Pan" },
+                new Command { Id = 1, HowTo = "Cut Bread", Line = "Get a Knife", Platform = "Knife & Chopping Board" },
+                new Command { Id = 2, HowTo = "Make Cup of Tea", Line = "Place Teabag in Cup", Platform = "Kettle & Cup" },
+            };
+
+            return commands;
+        }
+
+        public Command GetCommandById(int id)
+        {
+            return new Command{ Id = 0, HowTo = "Boil an Egg", Line = "Boil Water", Platform = "Kettle & Pan" };
+        }
+    }
+}
